9dfa3a924fadc649903ee3d3838e9e1a6bca940d
Nathaniel81 <nathanield356@gmail.com>
2024-06-02 22:31:55 +0300

    Update core models

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,58 +1,29 @@
 from django.db import models
 from accounts.models import User
+from cloudinary.models import CloudinaryField
 
 
-class AmenityType(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField()
+    title = models.CharField(max_length=200)
+    picture_url = models.URLField(null=True, blank=True)
 
-class PropertyType(models.Model):
-    name = models.CharField(max_length=100)
-
-class Room(models.Model):
-    name = models.CharField(max_length=200)
-    capacity = models.PositiveSmallIntegerField()
-    description = models.TextField(blank=True, null=True)
-
-class Country(models.Model):
-    name = models.CharField(max_length=100)
-
-class City(models.Model):
-    name = models.CharField(max_length=100)
-    state = models.ForeignKey(Country, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class Property(models.Model):
     host = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
-    available_start = models.DateField()
-    available_end = models.DateField()
+    guests = models.PositiveSmallIntegerField(default=0)
+    bedrooms = models.PositiveSmallIntegerField(default=0)
+    bathrooms = models.PositiveSmallIntegerField(default=0)
     photos = models.ManyToManyField('Photo')
-    property_type = models.ForeignKey(PropertyType, on_delete=models.SET_NULL, null=True)
-    rooms = models.ManyToManyField(Room)
-    amenities = models.ManyToManyField(AmenityType, through='Amenity')
-    guests = models.PositiveSmallIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-class Amenity(models.Model):
-    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='property_amenities')
-    amenity_type = models.ForeignKey(AmenityType, on_delete=models.CASCADE, default=None, related_name='amenity_type')
-
-class Booking(models.Model):
-    guest = models.ForeignKey(User, on_delete=models.CASCADE)
-    property = models.ForeignKey(Property, on_delete=models.CASCADE)
-    check_in_date = models.DateField()
-    check_out_date = models.DateField()
-    rooms = models.ManyToManyField(Room, through='RoomBooking')
-    total_guests = models.PositiveSmallIntegerField()
-
-class RoomBooking(models.Model):
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
-    adults = models.PositiveSmallIntegerField()
-    children = models.PositiveSmallIntegerField()
 
 class Review(models.Model):
     reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -61,9 +32,20 @@ class Review(models.Model):
     rating = models.PositiveSmallIntegerField()
 
 class Photo(models.Model):
-    url = models.ImageField()
-    alt_text = models.CharField(max_length=200)
+    picture = CloudinaryField('image', null=True, blank=True)
+    alt_text = models.CharField(max_length=200, null=True, blank=True)
 
 class Wishlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     properties = models.ManyToManyField(Property)
+
+class Address(models.Model):
+    continent = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    province = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.city}, {self.province}, {self.country}"
+
+    
