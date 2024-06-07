70686aea23d26cd185b985c2589d335a0eafbb25
Nathaniel81 <nathanield356@gmail.com>
2024-06-07 10:01:35 +0300

    Add function to generate country flag URL

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getFlagUrl(countryValue: string) {
+  // const flagBaseUrl = 'https://www.worldometers.info/img/flags/small/';
+  const flagUrl = `https://flagsapi.com/${countryValue}/shiny/64.png`;
+  // const flagUrl = `${flagBaseUrl}tn_${countryValue?.toLowerCase()}-flag.gif`;
+
+  return flagUrl;
+}
