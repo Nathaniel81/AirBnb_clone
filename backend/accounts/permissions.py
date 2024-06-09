8a7f3cd2d396827231d52ea38e591cf3813c2670
Nathaniel81 <nathanield356@gmail.com>
2024-06-09 17:19:50 +0300

    Update custom permission

--- backend/accounts/permissions.py
+++ backend/accounts/permissions.py
@@ -5,7 +5,9 @@ class AllowAnonymousGetPermission(permissions.BasePermission):
     """
     Custom permission to allow GET requests for anonymous users.
     """
+
     def has_permission(self, request, view):
-        if view.action == 'list' and request.method == 'GET':
+        if request.method in permissions.SAFE_METHODS:
             return True
-        return request.user.is_authenticated
+        else:
+            return request.user and request.user.is_authenticated
